a4cb48c676c1110c3e4afb0834ad32b0438da7c1
PulseLane <291593+PulseLane@users.noreply.github.com>
2020-08-17 12:04:56 -0700

    Init commit

---
+++ CampaignObjectiveParticleDisabler/Plugin.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using HarmonyLib;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace CampaignObjectiveParticleDisabler
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        public const string HarmonyId = "com.PulseLane.BeatSaber.CampaignObjectiveParticleDisabler";
+        internal static Harmony harmony => new Harmony(HarmonyId);
+
+        internal static Plugin instance { get; private set; }
+        internal static string Name => "CampaignObjectiveParticleDisabler";
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            instance = this;
+            Logger.log = logger;
+            Logger.log.Debug("Logger initialized.");
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            ApplyHarmonyPatches();
+        }
+        [OnDisable]
+        public void OnDisable()
+        {
+            RemoveHarmonyPatches();
+        }
+
+        public static void ApplyHarmonyPatches()
+        {
+            try
+            {
+                Logger.log.Debug("Applying Harmony patches.");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Logger.log.Critical("Error applying Harmony patches: " + ex.Message);
+                Logger.log.Debug(ex);
+            }
+        }
+
+        public static void RemoveHarmonyPatches()
+        {
+            try
+            {
+                // Removes all patches with this HarmonyId
+                harmony.UnpatchAll(HarmonyId);
+            }
+            catch (Exception ex)
+            {
+                Logger.log.Critical("Error removing Harmony patches: " + ex.Message);
+                Logger.log.Debug(ex);
+            }
+        }
+    }
+}
---
+++ CampaignObjectiveParticleDisabler/RefreshIconPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignObjectiveParticleDisabler
+{
+    [HarmonyPatch(typeof(MissionObjectiveGameUIView), "RefreshIcon")]
+    class RefreshIconPatch
+    {
+        static void Prefix(ref int ____numberOfParticles)
+        {
+            ____numberOfParticles = 0;
+        }
+    }
+}
